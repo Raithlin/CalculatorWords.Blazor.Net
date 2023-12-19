58ff753e070e3aef14fe53471d5635e55d8e0feb
Stephen Metcalfe <raithlin@outlook.com>
2023-12-19 20:01:53 +0200

    Change to TailwindCSS

---
+++ CalculatorWords/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.html',
+      './**/*.razor',
+      './**/*.cshtml',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
